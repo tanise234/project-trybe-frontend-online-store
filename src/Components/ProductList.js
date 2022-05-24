46c7c445c925cc9cf70d647b1ff7b71861d4c57a
Bruno Rodrigues <bruno.eheurod@gmail.com>
2022-05-24 16:38:27 -0300

    requisito 5

---
+++ src/Components/ProductList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+
+class ProductList extends Component {
+  render() {
+    return (
+      <div>
+        ProductList
+      </div>
+    );
+  }
+}
+
+// ProductList.propTypes = {
+
+// };
+
+export default ProductList;
