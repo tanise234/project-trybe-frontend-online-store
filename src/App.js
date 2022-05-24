5b5498d2405849288ea5a90aa93848bf1d814d95
Tanise Santos <tanise234@gmail.com>
2022-05-24 18:09:05 -0300

    requisito 4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
+import Categories from './Components/Categories';
 import Home from './Components/Home';
 
 function App() {
   return (
     <div>
-      <Home />
+      <main>
+        <Home />
+      </main>
+      <nav>
+        <Categories />
+      </nav>
     </div>
   );
 }
