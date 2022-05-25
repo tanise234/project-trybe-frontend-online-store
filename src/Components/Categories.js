8ef646a56b2cbfc3bb3184428f2445234914e026
Bruno Rodrigues <bruno.eheurod@gmail.com>
2022-05-25 16:27:08 -0300

    requisito 6
    Co-authored-by: Tanise Santos <tanise234@users.noreply.github.com>
    Co-authored-by: Matheus Santos Leão  <MatheusKRC@users.noreply.github.com>
    Co-authored-by: Lucas Gabriel Agostinho belgamann <LucasBelgamann@users.noreply.github.com>

--- src/Components/Categories.js
+++ src/Components/Categories.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { getCategories } from '../services/api';
 
 class Categories extends React.Component {
@@ -11,16 +12,30 @@ class Categories extends React.Component {
     this.setState({ list: setList });
   }
 
+  renderCategories(list, onClick) {
+    return list.map((item) => (
+      <div key={ item.id }>
+        <input
+          type="radio"
+          value={ item.id }
+          name="category"
+          data-testid="category"
+          onClick={ onClick }
+        />
+        <li data-testid="category">{item.name}</li>
+      </div>
+    ));
+  }
+
   render() {
     const { list } = this.state;
+    const { onClick } = this.props;
     return (
       <div>
         <h1>cat</h1>
         <ul>
           {
-            list.map((item) => (
-              <li key={ item.id } data-testid="category">{item.name}</li>
-            ))
+            this.renderCategories(list, onClick)
           }
         </ul>
       </div>
@@ -29,3 +44,7 @@ class Categories extends React.Component {
 }
 
 export default Categories;
+
+Categories.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
