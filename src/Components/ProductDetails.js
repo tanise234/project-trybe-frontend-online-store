060ac1dbf2d090feaf45a1d1b5d9b61e6ae3a990
LucasBelgamann <lucasbelgamann@hotmail.com>
2022-05-31 14:31:28 -0300

    salvar

--- src/Components/ProductDetails.js
+++ src/Components/ProductDetails.js
@@ -7,11 +7,18 @@ import CartButton from './CartButton';
 class ProductDetails extends React.Component {
   state = {
     product: {},
-  }
+    email: '',
+    rating: '',
+    desc: '',
+  };
   // const { addToCart } = this.props;
 
   async componentDidMount() {
-    const { match: { params: { id } } } = this.props;
+    const {
+      match: {
+        params: { id },
+      },
+    } = this.props;
     const url = `https://api.mercadolibre.com/items/${id}`;
     const getFetch = await fetch(url);
     const dataJson = await getFetch.json();
@@ -20,6 +27,15 @@ class ProductDetails extends React.Component {
 
   handleCartClick(cartItem) {
     addItem(cartItem);
+  }
+
+  handleEvaluation = () => {
+    this.setState({});
+  };
+
+  handleOnInput({ target: { name, type, checked, value } }) {
+    const checkedValue = type === 'checkbox' ? checked : value;
+    this.setState({ [name]: checkedValue });
   }
 
   render() {
@@ -40,9 +56,78 @@ class ProductDetails extends React.Component {
           onClick={ () => this.handleCartClick(product) }
         >
           Adicionar ao Carrinho
-
         </button>
         <CartButton />
+        <div>
+          <h4>Avaliação</h4>
+          <form>
+            <label htmlFor="input-email">
+              <input
+                type="email"
+                data-testid="product-detail-email"
+                value={ email }
+                name="email"
+                id="input-email"
+                onChange={ this.handleInput }
+              />
+            </label>
+            <div>
+              <label htmlFor="input-radio">
+                <input
+                  type="radio"
+                  id="input-radio"
+                  value={ rating }
+                  name="rating"
+                  data-testid={ `${index}-rating` }
+                />
+                <input
+                  type="radio"
+                  id="input-radio"
+                  value={ rating }
+                  name="rating"
+                  data-testid={ `${index}-rating` }
+                />
+                <input
+                  type="radio"
+                  id="input-radio"
+                  value={ rating }
+                  name="rating"
+                  data-testid={ `${index}-rating` }
+                />
+                <input
+                  type="radio"
+                  id="input-radio"
+                  value={ rating }
+                  name="rating"
+                  data-testid={ `${index}-rating` }
+                />
+                <input
+                  type="radio"
+                  id="input-radio"
+                  value={ rating }
+                  name="rating"
+                  data-testid={ `${index}-rating` }
+                />
+              </label>
+            </div>
+            <label htmlFor="text-area">
+              <textarea
+                data-testid="product-detail-evaluation"
+                value={ desc }
+                name="desc"
+                id="text-area"
+                onChange={ this.handleInput }
+              />
+            </label>
+            <button
+              type="button"
+              data-testid="submit-review-btn"
+              onClick={ this.handleEvaluation }
+            >
+              Mandar
+            </button>
+          </form>
+        </div>
       </div>
     );
   }
